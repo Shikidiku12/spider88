c60543651bbd055facd49616e03dab290dc065b1
Jayson Tipon-tipon <jayson@patolomius.com>
2024-01-29 19:15:20 +0800

    minor fix

--- resources/js/components/GamesList.jsx
+++ resources/js/components/GamesList.jsx
@@ -82,7 +82,7 @@ export const GamesList = ({
       if (!extractedValues) throw new Error('Invalid token');
 
       const formData = new FormData();
-      formData.append("username", `SPIDER88_${user.username}`);
+      formData.append("username", user.username);
       formData.append('password', extractedValues?.password);
 
       const response = await fetch(url, {
@@ -91,7 +91,7 @@ export const GamesList = ({
       });
       const body = await response.json();
 
-      console.log(body);
+      console.log(formData);
 
       const sessionToken = body.sessionToken.sessionToken;
 
