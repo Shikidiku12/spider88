3eeb19776c1159f04aded3164fdfe3c345f6aea2
Kenneth Fetalino <kenneth@patolomius.com>
2024-01-30 16:03:12 +0800

    chore: update

--- resources/js/components/GamesList.jsx
+++ resources/js/components/GamesList.jsx
@@ -94,7 +94,7 @@ export const GamesList = ({
 
       const gameLaunchParams = {
         gameCodeName: game.is_live ? game.code + ';' + game.alias : game.code,
-        username: Cookies.get('pas[flyingdragon88][real][username]'),
+        username: `SPIDER88_${user.username}`,
         tempToken: sessionToken,
         casino: 'flyingdragon88',
         clientPlatform: 'web',
